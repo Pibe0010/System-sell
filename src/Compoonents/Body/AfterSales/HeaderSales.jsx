a8c5ba4ca6ef57a65285d10eb6b171d2c1985eda
Crud-React-js <salamapatela@gmail.com>
2025-07-03 11:31:42 +0200

    insert style in header sales

--- src/Compoonents/Body/AfterSales/HeaderSales.jsx
+++ src/Compoonents/Body/AfterSales/HeaderSales.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { BtnOne, Clock, InputText2 } from "../../../index.js";
 import { v } from "../../../Styles/variables.jsx";
+import { Device } from "../../../Styles/BreakPionts.jsx";
 
 export const HeaderSales = () => {
   return (
@@ -15,7 +16,7 @@ export const HeaderSales = () => {
           </div>
           <div className="text-user">
             <span className="user">Carlos</span>
-            <span>Bookkeeper</span>
+            <span>🍱Bookkeeper</span>
           </div>
         </ContentUser>
         <article className="content-logo areaTwo">
@@ -32,7 +33,7 @@ export const HeaderSales = () => {
             <input className="form__field" type="text" placeholder="Search" />
           </InputText2>
         </article>
-        <article>
+        <article className="area-two">
           <BtnOne titulo="Reader" />
           <BtnOne titulo="Keyboard" />
         </article>
@@ -44,5 +45,83 @@ export const HeaderSales = () => {
 const Header = styled.div`
   grid-area: header;
   background-color: rgba(21, 248, 5, 0.5);
+  display: flex;
+  height: 100%;
+  border-bottom: 2px solid ${({ theme }) => theme.color2};
+  flex-direction: column;
+  .content-header {
+    width: 100%;
+    display: grid;
+    grid-template:
+      " areaOne areaTwo"
+      "areaThree areaThree";
+    .areaOne {
+      grid-area: areaOne;
+    }
+    .areaTwo {
+      grid-area: areaTwo;
+    }
+    .areaThree {
+      grid-area: areaThree;
+    }
+    @media ${Device.desktop} {
+      display: flex;
+      justify-content: space-between;
+    }
+    .content-logo {
+      display: flex;
+      align-items: center;
+      font-weight: 700;
+      img {
+        width: 30px;
+        object-fit: contain;
+      }
+    }
+  }
+  .search-content {
+    display: grid;
+    grid-template:
+      "area-two area-two"
+      "area-one area-one";
+    .area-one {
+      grid-area: area-one;
+    }
+    .area-two {
+      grid-area: area-two;
+      display: flex;
+      gap: 10px;
+    }
+    @media ${Device.desktop} {
+      display: flex;
+      gap: 10px;
+      .area-one {
+        width: 40vw;
+      }
+    }
+  }
 `;
-const ContentUser = styled.div``;
+const ContentUser = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  .contentImg {
+    display: flex;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    overflow: hidden;
+    img {
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+  .text-user {
+    display: flex;
+    flex-direction: column;
+    .user {
+      font-weight: 700;
+    }
+  }
+`;
